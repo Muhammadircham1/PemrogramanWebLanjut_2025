a6fe9529013ed06bbd5b4304293bf4d92fa59105
Muhammadircham1 <daffan245@gmail.com>
2025-03-06 16:18:51 +0700

    Praktikum-2.1

--- PWL2025_Jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
+++ PWL2025_Jobsheet4/PWL_POS/app/Http/Controllers/UserController.php
@@ -4,22 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
-use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
 
-        UserModel::create($data); 
-
-        $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
 }
